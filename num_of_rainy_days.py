7ebfb09366a3c8a7ff49dc72dcf86d73c4c6194e
Aakash Raj <aakash.raj@asu.edu>
2019-10-14 06:28:25 -0400

    Data Wrangling Info

---
+++ num_of_rainy_days.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 05:30:02 2019
+
+@author: Aakash Raj
+"""
+
+import pandas
+import pandasql
+
+filename = 'A:\DataScience\Intro_to_DS\weather-underground.csv'
+weather_data = pandas.read_csv(filename)
+
+q = """
+SELECT count(*) from weather_data WHERE rain = 1;
+"""
+    
+ #Execute your SQL command against the pandas frame
+rainy_days = pandasql.sqldf(q.lower(), locals())
+print(rainy_days)
+    
